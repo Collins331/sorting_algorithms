3550b833c49373fc55ef83b3193ac9c5d99c8df4
Collins <ochiengcollinsoduor2@gmail.com>
2023-10-21 14:44:01 +0300

    created 0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ * bubble_sort -  sorts an array of integers in ascending order
+ * @array: pointer to the array
+ * @size: the size of the array
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int x;
+	bool swapped;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = false;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				x = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = x;
+				swapped = true;
+				print_array(array, size);
+			}
+		}
+
+		if (swapped == false)
+			break;
+	}
+}
