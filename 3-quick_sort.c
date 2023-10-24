7807ba0625387c18562bac04fbc4fd4dd6645886
Collins <ochiengcollinsoduor2@gmail.com>
2023-10-24 18:04:17 +0300

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,8 @@
+#include "sort.h"
+/**
+* quick_sort -  sorts arrays though quick algorithm
+* @array: pointer to the array
+* @size: the size of the array
+* Return: void
+*/
+void quick_sort(int *array, size_t size)
